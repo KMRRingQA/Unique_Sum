47a806f7c5eb821f7671383dd3a5dfd5a65d8ea4
Korbinian <kmr.ring@outlook.com>
2020-03-27 18:16:46 +0000

    cleaning up variable names

--- src/com/qa/main/UniqueSum.java
+++ src/com/qa/main/UniqueSum.java
@@ -1,21 +1,21 @@
 package com.qa.main;
 
 public class UniqueSum {
-	int UniqueSummer(int Num1, int Num2, int Num3) {
-		if (Num1 == Num2 && Num2 == Num3) {
+	int UniqueSummer(int num1, int num2, int num3) {
+		if (num1 == num2 && num2 == num3) {
 			return 0;
 		}
-		if (Num1 == Num2) {
-			Num1 = Num2 = 0;
+		if (num1 == num2) {
+			num1 = num2 = 0;
 		}
-		if (Num1 == Num3) {
-			Num1 = Num3 = 0;	
+		if (num1 == num3) {
+			num1 = num3 = 0;
 		}
-		if (Num2 == Num3) {
-			Num2 = Num3 = 0;
+		if (num2 == num3) {
+			num2 = num3 = 0;
 		}
-		int sum = Num1 + Num2 + Num3;
-		return sum; 
+		int sum = num1 + num2 + num3;
+		return sum;
 	}
 
 }
